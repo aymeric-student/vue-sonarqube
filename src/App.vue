caef28d207f12021b74106d286616930ba8e6f4a
aymeric.maillot@efrei.net <66923802+Votybe@users.noreply.github.com>
2023-10-06 20:49:15 +0200

    add some text

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
+    <p>Je suis encore en train de tester !
+    </p>
     <p>Je suis en train de tester !</p>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
