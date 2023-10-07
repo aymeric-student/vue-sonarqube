1654aa513007325f4b30bc5441ce7dac21388492
aymeric.maillot@efrei.net <66923802+Votybe@users.noreply.github.com>
2023-10-07 18:22:19 +0200

    create bug

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,8 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <!-- Utilisation d'une propriété non définie -->
+    <p>{{ nonDefinedProperty }}</p>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
